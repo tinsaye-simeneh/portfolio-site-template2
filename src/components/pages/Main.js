1e255d98fd614668e96a647ff709c75a67b437cf
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-04 22:42:04 +0300

    first and second column created inside main.js

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import './index.css';
 
 let Main = () => {
     return (
-        <div className='container bg-primary'>
-        <h1 className='text-dark'>portfolio</h1>
+        <div className='container-fluid bg-black py-5'>
+            <div className='row py-5 mx-auto bg-warning w-75'>
+                <div className='col-4'>
+                    <h1 className='text-center text-white'>First Column</h1>
+                </div>
+                <div className='col-4'>
+                    <h1 className='text-center text-white'>Second Column</h1>
+                </div>
+            </div>
         </div>
     )
 }
