b812c66003e40df25760882a78779a17a2dbbb86
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-04 22:44:44 +0300

    thrid column created

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import './index.css';
+import '../../index.css';
 
 let Main = () => {
     return (
         <div className='container-fluid bg-black py-5'>
             <div className='row py-5 mx-auto bg-warning w-75'>
-                <div className='col-4'>
-                    <h1 className='text-center text-white'>First Column</h1>
+                <div className='col-4 bg-primary'>
+                    <p className='text-center text-white'>First Column</p>
                 </div>
-                <div className='col-4'>
-                    <h1 className='text-center text-white'>Second Column</h1>
+                <div className='col-4 bg-secondary'>
+                    <p className='text-center text-white'>Second Column</p>
+                </div>
+                <div className='col-4 bg-dark'>
+                    <p className='text-center text-white'>third Column</p>
                 </div>
             </div>
         </div>
