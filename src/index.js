60a0f313951dc1b83a955cd95b9bdcc07c3e09e2
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-04 20:48:31 +0300

    node modukes folder installed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Main from './components/page/Home'
+import Main from './components/page/Main'
 
 const App = () => {
   <>
