64ba317266d453cd2fd8919f02053cb74a4e2469
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-04 23:07:26 +0300

    third column text added

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -34,13 +34,24 @@ let Main = () => {
         <div className="col-3 ms-3 border-none rounded">
           <img src={Profile} alt="random" className="img-fluid" />
         </div>
-        <div className="col-4 bg-black pt-5 ms-5">
-          <h4 className="text-white">
+        <div className="col-5 bg-black pt-5 ms-5 mt-5">
+          <h4 className="text-white mt-3">
             Hi, I'm <span className="text-danger">Raul Josef</span>
           </h4>
           <h5 className="text-white mt-3">
             Frontend Web Developer | UI/UX Designer
           </h5>
+          <h6 className="text-white mt-3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et
+            justo id mauris laoreet porta. Orci varius natoque penatibus et
+            magnis dis parturient montes, nascetur ridiculus mus. Nunc lectus
+            felis, lacinia et iaculis sed, ornare in est. Aliquam euismod
+            sceleris que orci non congue. Etiam ac malesuada nulla. Maecenas
+            dapibus faucibus dignissim. Curabitur vestibulum mi egestas elit
+            tempus consequat. Phasellus non nisi et sem semper porttitor.
+            Interdum et malesuada fames ac ante ipsum primis in faucibus.
+            Curabitur.
+          </h6>
         </div>
       </div>
     </div>
