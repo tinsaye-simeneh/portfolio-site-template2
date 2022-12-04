2041b0e20e693432d4bfe9e52b4d49afde94ef04
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-04 22:32:58 +0300

    minor update

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 let Main = () => {
     return (
-        <div className='bg-primary'>
+        <div className='container bg-primary'>
         <h1 className='text-dark'>portfolio</h1>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App(){
   return(
   <Main/>
-  )
+  )simp
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
