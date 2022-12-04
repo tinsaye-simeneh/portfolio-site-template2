f132d6540dc83adfae1937b06d194f3cdbb302af
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-04 22:55:08 +0300

    profile image added

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -5,31 +5,31 @@ import Profile from "../../assets/Images/profile.png";
 let Main = () => {
   return (
     <div className="container-fluid bg-black py-5">
-      <div className="row py-5 mx-auto bg-warning w-75">
-        <div className="col-3">
+      <div className="row py-5 mx-auto bg-black w-75">
+        <div className="col-3 mt-5 pt-3">
           <div className="row">
-            <div className="col-3 mx-auto bg-danger">
+            <div className="col-3 mx-auto bg-warning">
               <h1 className="text-white">1</h1>
             </div>
           </div>
           <div className="row">
-            <div className="col-3 mx-auto bg-danger">
+            <div className="col-3 mx-auto bg-warning">
               <h1 className="text-white">1</h1>
             </div>
           </div>
           <div className="row">
-            <div className="col-3 mx-auto bg-danger">
+            <div className="col-3 mx-auto bg-warning">
               <h1 className="text-white">1</h1>
             </div>
           </div>
           <div className="row">
-            <div className="col-3 mx-auto bg-danger">
+            <div className="col-3 mx-auto bg-warning">
               <h1 className="text-white">1</h1>
             </div>
           </div>
         </div>
-        <div className="col-3 bg-secondary border rounded">
-            <img src="https://picsum.photos/200/300" alt="random" className="img-fluid"/>
+        <div className="col-3 border rounded">
+            <img src={Profile} alt="random" className="img-fluid"/>
         </div>
         <div className="col-5 bg-dark">
           <p className="text-center text-white">third Column</p>
