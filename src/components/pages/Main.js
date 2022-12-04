a5dc1cc3999865bbf0c4102e12d3a2df779cb78b
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-04 23:01:44 +0300

    first column and second column style fixed

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -4,10 +4,10 @@ import Profile from "../../assets/Images/profile.png";
 
 let Main = () => {
   return (
-    <div className="container-fluid bg-black py-5">
-      <div className="row py-5 mx-auto bg-black w-75">
-        <div className="col-3 mt-5 pt-3">
-          <div className="row">
+    <div className="container-fluid bg-black py-5" style={{height: '41.45rem'}}>
+      <div className="row py-5 mx-auto bg-black w-100">
+        <div className="col-3 mt-5 pt-5">
+          <div className="row mt-4">
             <div className="col-3 mx-auto bg-warning">
               <h1 className="text-white">1</h1>
             </div>
@@ -28,7 +28,7 @@ let Main = () => {
             </div>
           </div>
         </div>
-        <div className="col-3 border rounded">
+        <div className="col-3 ms-3 border-none rounded">
             <img src={Profile} alt="random" className="img-fluid"/>
         </div>
         <div className="col-5 bg-dark">
