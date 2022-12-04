d6c5434544151b5d2e9fa7d570f2c5b4937944f9
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-04 23:05:29 +0300

    third column text header added

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -4,7 +4,10 @@ import Profile from "../../assets/Images/profile.png";
 
 let Main = () => {
   return (
-    <div className="container-fluid bg-black py-5" style={{height: '41.45rem'}}>
+    <div
+      className="container-fluid bg-black py-5"
+      style={{ height: "41.45rem" }}
+    >
       <div className="row py-5 mx-auto bg-black w-100">
         <div className="col-3 mt-5 pt-5">
           <div className="row mt-4">
@@ -29,10 +32,15 @@ let Main = () => {
           </div>
         </div>
         <div className="col-3 ms-3 border-none rounded">
-            <img src={Profile} alt="random" className="img-fluid"/>
+          <img src={Profile} alt="random" className="img-fluid" />
         </div>
-        <div className="col-5 bg-dark">
-          <p className="text-center text-white">third Column</p>
+        <div className="col-4 bg-black pt-5 ms-5">
+          <h4 className="text-white">
+            Hi, I'm <span className="text-danger">Raul Josef</span>
+          </h4>
+          <h5 className="text-white mt-3">
+            Frontend Web Developer | UI/UX Designer
+          </h5>
         </div>
       </div>
     </div>
