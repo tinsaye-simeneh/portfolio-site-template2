ff7002176c3ec4f91c1d095c005a75bbf5a54b93
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-04 23:21:30 +0300

    minor edit

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -13,7 +13,7 @@ let Main = () => {
       style={{ height: "41.45rem" }}
     >
       <div className="row py-5 mx-auto bg-black w-100">
-        <div className="col-3 mt-5 pt-5">
+        <div className="col-3 mt-3 pt-5">
           <div className="row mt-5">
             <div className="col-3 mx-auto bg-orange py-3 pt-4 text-center">
               <img src={HomeIcon} alt="home" />
