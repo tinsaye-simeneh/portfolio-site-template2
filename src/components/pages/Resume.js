1011a9391860c073aa82356818e6c8a84b664123
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-05 19:29:56 +0300

    Resume page header added

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import SIdeBar from './SideBar';
-import '../../index.css'
+import React from "react";
+import SIdeBar from "./SideBar";
+import "../../index.css";
 
 const Resume = () => {
-    return(
-        <div className='container-fluid py-5 bg-black'>
-            <div className='row py-5'>
-                <SIdeBar/>
-                <div className='col-md-3'>
-                </div>
-            </div>
+  return (
+    <div className="container-fluid py-5 bg-black">
+      <div className="row py-5">
+        <SIdeBar />
+        <div
+          className="col-md-3 text-white text-center"
+          style={{ fontSize: "1.5rem" }}
+        >
+          Portfolio
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Resume;
