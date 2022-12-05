487592826cd50cd1531be00cae17e79353254fe8
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-05 20:39:08 +0300

    Work experience cards created

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -13,18 +13,39 @@ const Resume = () => {
 
         <div className="col-md-8 text-white" style={{ fontSize: "1.5rem" }}>
           <div className="row">
-            <span className="text-center mb-3">Resume</span>
+            <span className="text-center mb-3 text-decoration-underline">Resume</span>
           </div>
           <div className="row text-center mx-auto mt-3">
             <div className="col-md-6">
               Work Experience
-              <div className="col-md-6 mx-auto border rounded mt-5">sms.et</div>
-              <div className="col-md-6 mx-auto border rounded mt-5">sms.et</div>
+              <div className="col-md-10 bg-white text-black mx-auto border rounded mt-5 pt-2">
+                <span className="d-block" style={{fontSize: '1rem', fontWeight:'bold'}}>UI/UX Designer and Frontend Developer</span>
+                <span className="d-block" style={{fontSize: '1rem'}}>Remote - Parttime </span>
+                <span style={{fontSize: '1rem'}}>May, 2020 - Present</span>
+              </div>
+              <div className="col-md-10 bg-white text-black mx-auto border rounded mt-5 pt-2">
+                <span className="d-block" style={{fontSize: '1rem', fontWeight:'bold'}}>UI/UX Designer and Frontend Developer</span>
+                <span className="d-block" style={{fontSize: '1rem'}}>Remote - Parttime </span>
+                <span style={{fontSize: '1rem'}}>May, 2020 - Present</span>
+              </div>
+              <div className="col-md-10 bg-white text-black mx-auto border rounded mt-5 pt-2">
+                <span className="d-block" style={{fontSize: '1rem', fontWeight:'bold'}}>UI/UX Designer and Frontend Developer</span>
+                <span className="d-block" style={{fontSize: '1rem'}}>Remote - Parttime </span>
+                <span style={{fontSize: '1rem'}}>May, 2020 - Present</span>
+              </div>
             </div>
             <div className="col-md-6">
-            Education
-              <div className="col-md-6 mx-auto border rounded mt-5">sms.et</div>
-              <div className="col-md-6 mx-auto border rounded mt-5">sms.et</div>
+              Education
+              <div className="col-md-10 bg-white text-black mx-auto border rounded mt-5 pt-2">
+                <span className="d-block" style={{fontSize: '1rem', fontWeight:'bold'}}>ElectroMechanical Engineering</span>
+                <span className="d-block" style={{fontSize: '1rem'}}>Addis Ababa, Ethiopia </span>
+                <span style={{fontSize: '1rem'}}>May, 2020 - Present</span>
+              </div>
+              <div className="col-md-10 bg-white text-black mx-auto border rounded mt-5 pt-2">
+                <span className="d-block" style={{fontSize: '1rem', fontWeight:'bold'}}>UI/UX Designer and Frontend Developer</span>
+                <span className="d-block" style={{fontSize: '1rem'}}>Remote - Parttime </span>
+                <span style={{fontSize: '1rem'}}>May, 2020 - Present</span>
+              </div>
             </div>
           </div>
         </div>
