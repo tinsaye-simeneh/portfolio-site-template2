d95d50dc35da0a9754c1320e6d2f4ba252c06765
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-05 19:56:11 +0300

    portfolio sited cards first row created

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,35 +1,34 @@
 import React from "react";
 import SIdeBar from "./SideBar";
 import "../../index.css";
+import p1 from '../../assets/Images/p1.jpg'
 
 const Resume = () => {
   return (
     <div className="container-fluid py-5 bg-black">
-      <div className="row py-5">
+      <div className="row py-2">
         <SIdeBar />
 
         <div className="col-md-8 text-white" style={{ fontSize: "1.5rem" }}>
           <div className="row">
-            <span className="text-left">Portfolio</span>
+            <span className="text-left mb-3">Portfolio</span>
           </div>
-          <div className="row mt-3 text-center">
+          <div className="row text-center">
             <div className="col-md-6">
-              <div className="col-md-8 border rounded mt-5">yes</div>
-              <div className="col-md-8 border rounded mt-5">yes</div>
+              <div className="col-md-8 border rounded mt-3">
+                <img src={p1} alt="portoflio number 1" className="img-fluid"/>
+                sms.et
+              </div>
+              <div className="col-md-8 border rounded mt-5">
+              <img src={p1} alt="portoflio number 1" className="img-fluid"/>
+                sms.et
+              </div>
             </div>
             <div className="col-md-6">
               <div className="col-md-8 border rounded mt-5">yes</div>
               <div className="col-md-8 border rounded mt-5">yes</div>
             </div>
-          <div className="col-md-6">
-            <div className="col-md-8 border rounded mt-5">yes</div>
-            <div className="col-md-8 border rounded mt-5">yes</div>
-          </div>
-          <div className="col-md-6">
-            <div className="col-md-8 border rounded mt-5">yes</div>
-            <div className="col-md-8 border rounded mt-5">yes</div>
-          </div>
-          
+            
           </div>
         </div>
       </div>
