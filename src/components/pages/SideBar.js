e68c0191821b77be47cfa4d51193a44dbce9a4f6
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-05 09:39:28 +0300

    svg color changing

--- src/components/pages/SideBar.js
+++ src/components/pages/SideBar.js
@@ -13,22 +13,22 @@ const SIdeBar = () => {
     <div className="col-3 mt-3 pt-5">
       <div className="row mt-0">
         <div className="col-3 mx-auto bg-orange py-3 pt-5 text-center">
-          <img src={HomeIcon} alt="home" width={40}/>
+          <img src={HomeIcon} alt="home" width={40} className="filter-white"/>
         </div>
       </div>
       <div className="row">
         <div className="col-3 mx-auto bg-orange py-4 text-center">
-          <img src={Resume} alt="resume" width={40}/>
+          <img src={Resume} alt="resume" width={40} className="filter-black"/>
         </div>
       </div>
       <div className="row">
         <div className="col-3 mx-auto bg-orange py-4 text-center">
-          <img src={Portoflio} alt="Portfolio" width={40}/>
+          <img src={Portoflio} alt="Portfolio" width={40} className="filter-black"/>
         </div>
       </div>
       <div className="row">
         <div className="col-3 mx-auto bg-orange py-4 pb-5 text-center">
-          <img src={ContactIcon} alt="Contact" width={40}/>
+          <img src={ContactIcon} alt="Contact" width={40} className="filter-black"/>
         </div>
       </div>
       </div>
