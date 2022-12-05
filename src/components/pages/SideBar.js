e4c7c5f798474177c32d86bc4f8221486b83178a
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-05 09:33:44 +0300

    side bar icons changed

--- src/components/pages/SideBar.js
+++ src/components/pages/SideBar.js
@@ -2,33 +2,33 @@ import React from "react";
 
 import "../../index.css";
 import Profile from "../../assets/Images/profile.png";
-import HomeIcon from "../../assets/icons/home.png";
-import ContactIcon from "../../assets/icons/contact.png";
-import Resume from "../../assets/icons/resume.png";
-import Portoflio from "../../assets/icons/portfolio.png";
+import HomeIcon from "../../assets/icons/home.svg";
+import ContactIcon from "../../assets/icons/contact.svg";
+import Resume from "../../assets/icons/resume.svg";
+import Portoflio from "../../assets/icons/portfolio.svg";
 
 const SIdeBar = () => {
   return (
     <>
     <div className="col-3 mt-3 pt-5">
-      <div className="row mt-5">
-        <div className="col-3 mx-auto bg-orange py-3 pt-4 text-center">
-          <img src={HomeIcon} alt="home" />
+      <div className="row mt-0">
+        <div className="col-3 mx-auto bg-orange py-3 pt-5 text-center">
+          <img src={HomeIcon} alt="home" width={40}/>
         </div>
       </div>
       <div className="row">
-        <div className="col-3 mx-auto bg-orange py-3 text-center">
-          <img src={Resume} alt="resume" />
+        <div className="col-3 mx-auto bg-orange py-4 text-center">
+          <img src={Resume} alt="resume" width={40}/>
         </div>
       </div>
       <div className="row">
-        <div className="col-3 mx-auto bg-orange py-3 text-center">
-          <img src={Portoflio} alt="Portfolio" />
+        <div className="col-3 mx-auto bg-orange py-4 text-center">
+          <img src={Portoflio} alt="Portfolio" width={40}/>
         </div>
       </div>
       <div className="row">
-        <div className="col-3 mx-auto bg-orange py-3 pb-4 text-center">
-          <img src={ContactIcon} alt="Contact" />
+        <div className="col-3 mx-auto bg-orange py-4 pb-5 text-center">
+          <img src={ContactIcon} alt="Contact" width={40}/>
         </div>
       </div>
       </div>
