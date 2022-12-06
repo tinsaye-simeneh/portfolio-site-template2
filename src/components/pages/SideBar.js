c051cb5f269fb82a0e66c13ffef314bebe5576f2
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-06 09:44:08 +0300

    Sidebar menu routing

--- src/components/pages/SideBar.js
+++ src/components/pages/SideBar.js
@@ -6,6 +6,7 @@ import HomeIcon from "../../assets/icons/home.svg";
 import ContactIcon from "../../assets/icons/contact.svg";
 import Resume from "../../assets/icons/resume.svg";
 import Portoflio from "../../assets/icons/portfolio.svg";
+import { Outlet, Link } from "react-router-dom";
 
 const SIdeBar = ({ marginTop }) => {
   return (
@@ -18,32 +19,40 @@ const SIdeBar = ({ marginTop }) => {
           RJ
         </div>
         <div className="col-3 mx-auto bg-orange py-3 pt-5 text-center">
+        <Link to="/Home">
           <img src={HomeIcon} alt="home" width={40} className="filter-white" />
+          </Link>
         </div>
       </div>
       <div className="row">
         <div className="col-3 mx-auto bg-orange py-4 text-center">
+        <Link to="/Resume">
           <img src={Resume} alt="resume" width={40} className="filter-black" />
+          </Link>
         </div>
       </div>
       <div className="row">
         <div className="col-3 mx-auto bg-orange py-4 text-center">
+        <Link to="/Portfolio">
           <img
             src={Portoflio}
             alt="Portfolio"
             width={40}
             className="filter-black"
           />
+          </Link>
         </div>
       </div>
       <div className="row">
         <div className="col-3 mx-auto bg-orange py-4 pb-5 text-center">
+        <Link to="/Contact">
           <img
             src={ContactIcon}
             alt="Contact"
             width={40}
             className="filter-black"
           />
+          </Link>
         </div>
       </div>
     </>
