71a289023afe06f4f4c77d18f100008e5ad41bbd
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-06 11:13:46 +0300

    side bar menu icons hover effect

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -9,43 +9,89 @@ const Resume = () => {
         <div className="col-3 mt-2 pt-5">
           <SIdeBar />
         </div>
-
         <div className="col-md-8 text-white" style={{ fontSize: "1.5rem" }}>
           <div className="row">
-            <span className="text-center mb-3 text-decoration-underline">Resume</span>
+            <span className="text-center mb-3 text-decoration-underline">
+              Resume
+            </span>
           </div>
           <div className="row text-center mx-auto mt-3">
             <div className="col-md-6">
               Work Experience
               <div className="col-md-10 bg-white text-black mx-auto border rounded mt-5 pt-2">
-                <span className="d-block" style={{fontSize: '1rem', fontWeight:'bold'}}>UI/UX Designer and Frontend Developer</span>
-                <span className="d-block" style={{fontSize: '1rem'}}>Remote - Parttime </span>
-                <span style={{fontSize: '1rem'}}>May, 2020 - Present</span>
+                <span
+                  className="d-block"
+                  style={{ fontSize: "1rem", fontWeight: "bold" }}
+                >
+                  UI/UX Designer and Frontend Developer
+                </span>
+                <span className="d-block" style={{ fontSize: "1rem" }}>
+                  Remote - Parttime{" "}
+                </span>
+                <span style={{ fontSize: "1rem" }}>May, 2020 - Present</span>
               </div>
               <div className="col-md-10 bg-white text-black mx-auto border rounded mt-5 pt-2">
-                <span className="d-block" style={{fontSize: '1rem', fontWeight:'bold'}}>UI/UX Designer and Frontend Developer</span>
-                <span className="d-block" style={{fontSize: '1rem'}}>Remote - Parttime </span>
-                <span style={{fontSize: '1rem'}}>May, 2020 - Present</span>
+                <span
+                  className="d-block"
+                  style={{ fontSize: "1rem", fontWeight: "bold" }}
+                >
+                  UI/UX Designer and Frontend Developer
+                </span>
+                <span className="d-block" style={{ fontSize: "1rem" }}>
+                  Remote - Parttime{" "}
+                </span>
+                <span style={{ fontSize: "1rem" }}>May, 2020 - Present</span>
               </div>
               <div className="col-md-10 bg-white text-black mx-auto border rounded mt-5 pt-2">
-                <span className="d-block" style={{fontSize: '1rem', fontWeight:'bold'}}>UI/UX Designer and Frontend Developer</span>
-                <span className="d-block" style={{fontSize: '1rem'}}>Remote - Parttime </span>
-                <span style={{fontSize: '1rem'}}>May, 2020 - Present</span>
+                <span
+                  className="d-block"
+                  style={{ fontSize: "1rem", fontWeight: "bold" }}
+                >
+                  UI/UX Designer and Frontend Developer
+                </span>
+                <span className="d-block" style={{ fontSize: "1rem" }}>
+                  Remote - Parttime{" "}
+                </span>
+                <span style={{ fontSize: "1rem" }}>May, 2020 - Present</span>
               </div>
             </div>
             <div className="col-md-6">
               Education
               <div className="col-md-10 bg-white text-black mx-auto border rounded mt-5 pt-2">
-                <span className="d-block" style={{fontSize: '1rem', fontWeight:'bold'}}>ElectroMechanical Engineering</span>
-                <span className="d-block" style={{fontSize: '1rem', fontWeight:'bold'}}>AASTU</span>
-                <span className="d-block" style={{fontSize: '1rem'}}>Addis Ababa, Ethiopia </span>
-                <span style={{fontSize: '1rem'}}>May, 2020 - Present</span>
+                <span
+                  className="d-block"
+                  style={{ fontSize: "1rem", fontWeight: "bold" }}
+                >
+                  ElectroMechanical Engineering
+                </span>
+                <span
+                  className="d-block"
+                  style={{ fontSize: "1rem", fontWeight: "bold" }}
+                >
+                  AASTU
+                </span>
+                <span className="d-block" style={{ fontSize: "1rem" }}>
+                  Addis Ababa, Ethiopia{" "}
+                </span>
+                <span style={{ fontSize: "1rem" }}>May, 2020 - Present</span>
               </div>
               <div className="col-md-10 bg-white text-black mx-auto border rounded mt-5 pt-2">
-                <span className="d-block" style={{fontSize: '1rem', fontWeight:'bold'}}>ElectroMechanical Engineering</span>
-                <span className="d-block" style={{fontSize: '1rem', fontWeight:'bold'}}>AASTU</span>
-                <span className="d-block" style={{fontSize: '1rem'}}>Addis Ababa, Ethiopia </span>
-                <span style={{fontSize: '1rem'}}>May, 2020 - Present</span>
+                <span
+                  className="d-block"
+                  style={{ fontSize: "1rem", fontWeight: "bold" }}
+                >
+                  ElectroMechanical Engineering
+                </span>
+                <span
+                  className="d-block"
+                  style={{ fontSize: "1rem", fontWeight: "bold" }}
+                >
+                  AASTU
+                </span>
+                <span className="d-block" style={{ fontSize: "1rem" }}>
+                  Addis Ababa, Ethiopia{" "}
+                </span>
+                <span style={{ fontSize: "1rem" }}>May, 2020 - Present</span>
               </div>
             </div>
           </div>
--- src/components/pages/SideBar.js
+++ src/components/pages/SideBar.js
@@ -1,9 +1,9 @@
 import React from "react";
-
 import "../../index.css";
 import { Link } from "react-router-dom";
 
 const SIdeBar = () => {
+
   return (
     <>
       <div className="row mt-0">
@@ -20,7 +20,7 @@ const SIdeBar = () => {
               width="30"
               height="30"
               fill="currentColor"
-              class="bi bi-house-door-fill active"
+              class="bi bi-house-door-fill active menu-icon"
               viewBox="0 0 16 16"
             >
               <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z" />
@@ -36,7 +36,7 @@ const SIdeBar = () => {
               width="30"
               height="30"
               fill="currentColor"
-              class="bi bi-file-earmark-person-fill"
+              class="bi bi-file-earmark-person-fill menu-icon"
               viewBox="0 0 16 16"
             >
               <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0zm2 5.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-.245S4 12 8 12s5 1.755 5 1.755z" />
@@ -52,7 +52,7 @@ const SIdeBar = () => {
               width="30"
               height="30"
               fill="currentColor"
-              class="bi bi-person-workspace"
+              class="bi bi-person-workspace menu-icon"
               viewBox="0 0 16 16"
             >
               <path d="M4 16s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H4Zm4-5.95a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
@@ -69,7 +69,7 @@ const SIdeBar = () => {
               width="30"
               height="30"
               fill="currentColor"
-              class="bi bi-person-lines-fill"
+              class="bi bi-person-lines-fill menu-icon"
               viewBox="0 0 16 16"
             >
               <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z" />
