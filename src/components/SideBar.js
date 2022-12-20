fc8b7b5cade8b8443c1abd178ed6a2de37eec93e
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-20 06:11:18 +0300

    Sidebar bug fixed

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,47 +1,47 @@
 import React from "react";
 import "../style/index.css";
 import { Link } from "react-router-dom";
-import {AiFillHome} from 'react-icons/ai'
-import {AiFillFilePdf} from 'react-icons/ai'
-import {MdContactPage } from 'react-icons/md'
-import {GiGiftOfKnowledge} from 'react-icons/gi'
+import { AiFillHome } from "react-icons/ai";
+import { AiFillFilePdf } from "react-icons/ai";
+import { MdContactPage } from "react-icons/md";
+import { GiGiftOfKnowledge } from "react-icons/gi";
 
-const SIdeBar = ({Home, Resume, Portfolio, Contact}) => {
-
+const SIdeBar = ({ Home, Resume, Portfolio, Contact }) => {
   return (
     <>
-      <div className="row mt-0">
+      <div className="row">
         <div
           className="bg-black text-center pb-1 text-decoration-underline text-orange"
           style={{ fontSize: "2rem" }}
         >
           RJ
         </div>
-        <div className="col-md-3 col-3 sidebar-item mx-auto bg-orange py-3 pt-5 text-center">
-          <Link to="/Home">
-           <AiFillHome size={30}/>
-          </Link>
-        </div>
       </div>
-      <div className="row">
-        <div className="col-md-3 col-3 sidebar-item mx-auto bg-orange py-4 text-center ">
-          <Link to="/Resume">
-            <AiFillFilePdf size={30}/>
-          </Link>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-md-3 col-3 sidebar-item mx-auto bg-orange py-4 text-center">
-          <Link to="/Portfolio">
-           <GiGiftOfKnowledge size={30}/>
-          </Link>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-md-3 col-3 sidebar-item mx-auto bg-orange pt-3 pb-5 text-center">
+
+      <div className="row bg-primary text-center">
+        <div className="col-md-3 col-12 mx-auto bg-orange py-4 px-3 text-center">
+          <div className="row">
+            <div className="col-md-12 col-3"> <div className="col-md-3 col-12 mx-auto bg-orange py-4 text-center">
           <Link to="/Contact">
-           <MdContactPage size={30}/>
+            <MdContactPage size={30} />
           </Link>
+        </div></div>
+            <div className="col-md-12 col-3"> <div className="col-md-3 col-12 mx-auto bg-orange py-4 text-center">
+          <Link to="/Contact">
+            <MdContactPage size={30} />
+          </Link>
+        </div></div>
+            <div className="col-md-12 col-3"> <div className="col-md-3 col-12 mx-auto bg-orange py-4 text-center">
+          <Link to="/Contact">
+            <MdContactPage size={30} />
+          </Link>
+        </div></div>
+            <div className="col-md-12 col-3"> <div className="col-md-3 col-12 mx-auto bg-orange py-4 text-center">
+          <Link to="/Contact">
+            <MdContactPage size={30} />
+          </Link>
+        </div></div>
+          </div>
         </div>
       </div>
     </>
