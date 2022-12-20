ccc4c2ea6bc7d3425167ea237a3d2b4741e3c3ba
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-20 10:50:23 +0300

    Portfolio project image and caption changed

--- src/components/PortfolioCards.js
+++ src/components/PortfolioCards.js
@@ -1,21 +1,21 @@
 import React from "react";
-import p1 from "../assets/Images/p1.jpg";
+import PortfolioImg from "../assets/Images/portfolio.jpg";
 import '../style/index.css'
 
 const Cards = () => {
   return (
     <div className="col-md-4">
       <div className="col-md-6 mx-auto border rounded mt-3 img-card">
-        <img src={p1} alt="portoflio number 1" className="img-fluid" />
-        sms.et
+        <img src={PortfolioImg} alt="portoflio number 1" className="img-fluid" />
+        Project Name
       </div>
       <div className="col-md-6 mx-auto border rounded mt-5">
-        <img src={p1} alt="portoflio number 1" className="img-fluid" />
-        sms.et
+        <img src={PortfolioImg} alt="portoflio number 1" className="img-fluid" />
+        Project Name
       </div>
       <div className="col-md-6 mx-auto border rounded mt-5">
-        <img src={p1} alt="portoflio number 1" className="img-fluid" />
-        sms.et
+        <img src={PortfolioImg} alt="portoflio number 1" className="img-fluid" />
+        Project Name
       </div>
     </div>
   );
